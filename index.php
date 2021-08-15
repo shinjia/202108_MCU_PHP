34a78dab2dd8b30ccb3d90d5d5ea181769868c3f
Shinjia Chen <shinjia168@gmail.com>
2021-08-15 15:40:57 +0800

    Class02

--- index.php
+++ index.php
@@ -3,10 +3,35 @@
 <head>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<title>Title of the document</title>
+<title>PHP+MySQL</title>
 </head>
 <body>
-<h1>Hello World</h1>
+<h1>PHP+MySQL 老師上課實作範例</h1>
+
+
+<h2>Class 02 (2021.08.15 下午)</h2>
+
+<p>九九乘法練習 (game99)</p>
+<ul>
+  <li>練習圖檔下載：<a href="game99/images_game99.zip">images_game99.zip</a></li>
+  <li>程式執行 <a href="game99/question.php">question.php</a> </li>
+  <li>查看原始碼：出題 [<a href="show_source.php?dir=game99&amp;file=question.php">question.php</a>]</li>
+  <li>查看原始碼：顯示答案 [<a href="show_source.php?dir=game99&amp;file=answer.php">answer.php</a>]</li>
+</ul>
+
+<p>在網頁上也會出現 GET 傳遞資料用法</p>
+<ul>
+  <li><a href="https://www.google.com.tw/search?q=php">查看更多『php』的資料</a></li>
+  <li><a href="https://www.google.com.tw/search?q=程式">查看更多『程式』的資料</a></li>
+</ul>
+
+<p>上週作業解答 (rand_pic)</p>
+<ul>
+  <li>程式執行 <a href="rand_pic/index.php">rand_pic/index.php</a></li>
+</ul>
+
+<p>&nbsp;</p>
+
 
 
 <h2>Class 01 (2021.08.15 上午)</h2>
